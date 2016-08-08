c718c57192076730d145ce9a73616a2fe7767999
Dominik Vesely <dominik.vesely@ackee.cz>
2016-08-08 18:01:58 +0200

    all pods included

---
+++ Example/Pods/Target Support Files/Pods-ReactiveLocation_Tests/Pods-ReactiveLocation_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_ReactiveLocation_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ReactiveLocation_TestsVersionString[];
+
---
+++ Example/Pods/Target Support Files/ReactiveLocation/ReactiveLocation-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ReactiveLocation : NSObject
+@end
+@implementation PodsDummy_ReactiveLocation
+@end
