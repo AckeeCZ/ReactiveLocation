b88dfd7eb9e7442ecf163bef23e17530ed0e5bbc
Jakub Olejník <jakub.olejnik@ackee.cz>
2017-12-27 21:48:11 +0100

    Reacreate project structure

---
+++ ReactiveLocation/Supporting files/ReactiveLocation.h
@@ -0,0 +1,18 @@
+//
+//  ReactiveLocation.h
+//  ReactiveLocation
+//
+//  Created by Jakub Olejník on 27/12/2017.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReactiveLocation.
+FOUNDATION_EXPORT double ReactiveLocationVersionNumber;
+
+//! Project version string for ReactiveLocation.
+FOUNDATION_EXPORT const unsigned char ReactiveLocationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReactiveLocation/PublicHeader.h>
+
+
